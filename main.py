efd9299d8e5e84871ba8def40a422d5111b33e07
g1serra <giuseppe.serra@outlook.com>
2023-06-02 00:31:53 +0200

    2023-06-02 00:31:53

---
+++ main.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+header = st.container()
+dataset = st.container()
+features = st.container()
+modelTraining = st.container()
+
+with header:
+    st.title("welcome to my first Streamlit app")
+
+
